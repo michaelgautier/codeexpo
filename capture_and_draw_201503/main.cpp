f10a38d41cfbea521fc5938360d4481f966593ae
Michael Gautier <michaelqgautier@gmail.com>
2015-04-20 04:15:19 -0500

    fixed name from the archival name to the actual working name.

--- capture_and_draw_201503/main.cpp
+++ capture_and_draw_201503/main.cpp
@@ -1,3 +1,19 @@
+/*
+	Copyright 2015 Michael Gautier
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 /*Includes: Standard Library*/
 #include <algorithm>
 #include <vector>
@@ -571,20 +587,4 @@ auto main() -> int
 		 767024 Mar 27 14:34 program.o	debug
 		1396436 Mar 27 14:34 assembled executable	debug
 
-*/
-
-/*
-	Copyright 2015 Michael Gautier
-
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
 */
